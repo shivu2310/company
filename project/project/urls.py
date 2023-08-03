e55b588f7c5d5b61741cfe33037a008c1c9e73ef
shivam bhargava <shivudevbhargava@gmail.com>
2023-08-03 22:40:07 +0530

    commit

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,6 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app1/',include('django.contrib.auth.urls')),
+    # path('app1/',include('django.contrib.auth.urls')),
     path('',include('app1.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
